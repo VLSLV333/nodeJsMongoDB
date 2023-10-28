cbad18b7427aae1a23d7a4085b95f9a9d6716594
Vladyslav <vlad.shrst@gmail.com>
2023-10-28 16:18:20 +0300

    add mongoDb data managment, delete some models, leave only product and user models. User model now contains methods to create cart, clear and update cart, also creating order method. Controllers and some views change according to new models methods. MongoDb looks like more "JS oriented" db, like it so far

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-'Just a begining';
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const show404 = require('./controllers/404').show404;
+
+const connectToMongo = require('./util/databaseMongoDb').connectToMongo;
+
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('653787b412fdc1cd464f0534')
+    .then((user) => {
+      req.user = new User(user.name, user.email, user._id, user.cart); // by this we create new field in req object, so req.user was undefined, now we've stored user from our db
+      //  in our req object req.user = user. Also the 'user' we've saved her is the object created by sequelize with all it's methods
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use('/', show404);
+
+connectToMongo(() => {
+  app.listen(3000);
+});
